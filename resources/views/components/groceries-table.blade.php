5501620f421fe8891b30f452d1eb18ae3c2e2082
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2022-10-24 12:26:03 +0700

    (feat): create table for list of groceries

---
+++ resources/views/components/groceries-table.blade.php
@@ -0,0 +1,34 @@
+<div class="overflow-x-auto relative shadow-md sm:rounded-lg mt-4">
+  <table class="w-full text-sm text-left text-gray-500 ">
+    <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
+      <tr>
+        <th scope="col" class="p-4">
+					Check
+        </th>
+        <th scope="col" class="py-3 px-6">
+          Item
+        </th>
+        <th scope="col" class="py-3 px-6">
+          Nominal
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="bg-white border-b hover:bg-gray-50 0">
+        <td class="p-4 w-4">
+          <div class="flex items-center">
+            <input id="checkbox-table-search-1" type="checkbox"
+              class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 focus:ring-2">
+            <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+          </div>
+        </td>
+        <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap">
+          Apple MacBook Pro 17"
+        </th>
+        <td class="py-4 px-6">
+          Sliver
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-  @slot('styles')
+  <x-slot name="styles">
     <style>
       input[type='number']::-webkit-inner-spin-button,
       input[type='number']::-webkit-outer-spin-button {
@@ -15,44 +15,48 @@
         outline: none !important;
       }
     </style>
-  @endslot
+  </x-slot>
 
   <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 bg-white border-b border-gray-200">
           <form action="" class="space-y-4">
-           <div class="flex items-center justify-between">
-                <div class="form-control w-3/4">
-                  <label for="default-input" class="block mb-2 text-sm font-medium text-black">Grocery Item</label>
-                  <input type="text" id="default-input" name="name"
-                    class="bg-gray-50 border border-gray-200 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+            <div class="flex items-center justify-between">
+              <div class="form-control w-3/4">
+                <label for="default-input" class="block mb-2 text-sm font-medium text-black">Grocery Item</label>
+                <input type="text" id="default-input" name="name"
+                  class="bg-gray-50 border border-gray-200 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+              </div>
+              <div class="custom-number-input w-32 mr-4 flex items-center flex-col">
+                <label for="default-input" class="w-full text-sm font-medium text-black">Nominal</label>
+                <div class="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">
+                  <button data-action="decrement"
+                    class=" bg-gray-50 border border-gray-200 text-gray-900 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none">
+                    <span class="m-auto text-2xl font-thin">−</span>
+                  </button>
+                  <input type="number"
+                    class="outline-none focus:outline-none text-center w-full bg-gray-50 border border-gray-200 text-gray-900 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700  outline-none"
+                    name="nominal" value="0" min="0"></input>
+                  <button data-action="increment"
+                    class="bg-gray-50 border border-gray-200 text-gray-900 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer">
+                    <span class="m-auto text-2xl font-thin">+</span>
+                  </button>
                 </div>
-                <div class="custom-number-input w-32 mr-4 flex items-center flex-col">
-                  <label for="default-input" class="w-full text-sm font-medium text-black">Nominal</label>
-                  <div class="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">
-                    <button data-action="decrement"
-                      class=" bg-gray-50 border border-gray-200 text-gray-900 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none">
-                      <span class="m-auto text-2xl font-thin">−</span>
-                    </button>
-                    <input type="number"
-                      class="outline-none focus:outline-none text-center w-full bg-gray-50 border border-gray-200 text-gray-900 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700  outline-none"
-                      name="nominal" value="0" min="0"></input>
-                    <button data-action="increment"
-                      class="bg-gray-50 border border-gray-200 text-gray-900 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer">
-                      <span class="m-auto text-2xl font-thin">+</span>
-                    </button>
-                  </div>
-                </div>
-           </div>
-            <button type="submit" class="w-24 rounded bg-blue-400 py-2 px-1 font-semibold text-white hover:bg-blue-500 transition duration-1">Add</button>
+              </div>
+            </div>
+            <button type="submit"
+              class="w-24 rounded bg-blue-400 py-2 px-1 font-semibold text-white hover:bg-blue-500 transition duration-1">Add</button>
           </form>
         </div>
+		<div class="bg-white overflow-hidden shadow-sm">
+			<x-groceries-table />
+		</div>
       </div>
     </div>
   </div>
 
-  @slot('scripts')
+  <x-slot name="scripts">
     <script>
       function decrement(e) {
         e.preventDefault()
@@ -93,5 +97,5 @@
         btn.addEventListener("click", increment);
       });
     </script>
-  @endslot
+  </x-slot>
 </x-app-layout>
