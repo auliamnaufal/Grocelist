2c7fe36ad85e9a884e5c4bff6847ddb941c2a914
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2022-10-26 11:29:43 +0700

    (refactor): remove check on deleted item section

--- resources/views/components/groceries-table.blade.php
+++ resources/views/components/groceries-table.blade.php
@@ -3,9 +3,11 @@
   <table class="w-full text-sm text-left text-gray-500 ">
     <thead class="text-xs text-gray-700 uppercase bg-gray-100 ">
       <tr>
-        <th scope="col" class="p-4">
-          Check
-        </th>
+        @if ($position != 'deleted')
+          <th scope="col" class="p-4">
+            Check
+          </th>
+        @endif
         <th scope="col" class="py-3 px-6">
           Item
         </th>
@@ -20,14 +22,16 @@
     <tbody>
       @foreach ($groceries as $grocery)
         <tr class="bg-white border-b hover:bg-gray-50">
-          <td class="p-4 w-4">
-            <div class="flex items-center">
-              <input id="checkbox-table-search-1" type="checkbox" {{ $grocery->checked == 1 ? 'checked' : '' }}
-                onclick="document.getElementById('toggle-check-{{ $grocery->id }}').submit()"
-                class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 focus:ring-2">
-              <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
-            </div>
-          </td>
+          @if ($position != 'deleted')
+            <td class="p-4 w-4">
+              <div class="flex items-center">
+                <input id="checkbox-table-search-1" type="checkbox" {{ $grocery->checked == 1 ? 'checked' : '' }}
+                  onclick="document.getElementById('toggle-check-{{ $grocery->id }}').submit()"
+                  class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 focus:ring-2">
+                <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+              </div>
+            </td>
+          @endif
           <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap">
             {{ $grocery->name }}
           </th>
@@ -46,7 +50,8 @@
               <form action="/groceries/{{ $grocery->id }}/restore" method="POST">
                 @csrf
                 <button type="submit"
-                  class="border-none bg-transparent text-blue-400 hover:text-blue-500 transition duration-.5">Put Back</button>
+                  class="border-none bg-transparent text-blue-400 hover:text-blue-500 transition duration-.5">Put
+                  Back</button>
               </form>
             @endif
           </td>
